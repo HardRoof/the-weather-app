bcf01326b200afd79c94cbd5720d43b749c11b54
HardRoof <mateus_grandez@hotmail.com>
2022-09-09 18:22:15 -0600

    Added a parameter to showMainCard function

--- src/modules/view/mainView.js
+++ src/modules/view/mainView.js
@@ -1,10 +1,10 @@
 class View {
   constructor() {}
 
-  showMainCard() {
-    const temp = document.getElementsByTagName("template")[0];
-    const clon = temp.content.cloneNode(true);
-    document.getElementsByClassName("container")[0].append(clon);
+  showMainCard(position) {
+    const temp = document.getElementsByTagName("template")[position];
+    const clone = temp.content.cloneNode(true);
+    document.getElementsByClassName("container")[0].append(clone);
   }
 }
 
