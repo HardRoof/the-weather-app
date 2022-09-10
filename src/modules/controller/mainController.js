652abe16b76bb5137b3c9304639027227cc24979
HardRoof <mateus_grandez@hotmail.com>
2022-09-09 18:22:42 -0600

    Added functions to generate 8days forecast

--- src/modules/controller/mainController.js
+++ src/modules/controller/mainController.js
@@ -12,18 +12,21 @@ class Controller {
   }
 
   async searchWeather(e) {
-    const WeatherData = await this.getWeatherData(e);
-    this.view.showMainCard();
-    this.createMainCard(WeatherData.currentWeather);
+    const weatherData = await this.getWeatherData(e);
+    this.view.showMainCard(0);
+    console.log(weatherData.currentWeather);
+    this.createMainCard(weatherData.currentWeather);
+    this.view.showMainCard(1);
+    this.createEightDaysCard(weatherData.eightDaysWeather);
   }
 
   async getWeatherData(e) {
     e.preventDefault();
     const geoLocation = await this.model.geoCode(this.getElements().input.value);
     const currentWeather = await this.model.getCurrentWeather(geoLocation.lat, geoLocation.lon);
-    const fiveDayWeather = await this.model.getFiveDayWeather(geoLocation.lat, geoLocation.lon);
+    const eightDaysWeather = await this.model.getEightDaysWeather(geoLocation.lat, geoLocation.lon);
 
-    return { geoLocation, currentWeather, fiveDayWeather };
+    return { geoLocation, currentWeather, eightDaysWeather };
   }
 
   createMainCard(currentWeather) {
@@ -36,6 +39,11 @@ class Controller {
     const humidity = "HUMIDITY: " + currentWeather.main.humidity + "%";
     const mainCard = this.model.generateMainCard(condition, city, degrees, feelsLike, wind, humidity);
     mainCard.fillMainCard();
+  }
+
+  createEightDaysCard(eightDaysWeather) {
+    const eightDaysCard = this.model.generateEightDaysCard(eightDaysWeather);
+    eightDaysCard.fillEightDaysCard();
   }
 }
 export default Controller;
