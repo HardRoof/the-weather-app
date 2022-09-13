0d33bd5af9ebfac86bf9b5231ff78fb51bd34c54
HardRoof <mateus_grandez@hotmail.com>
2022-09-12 18:19:16 -0600

    Created function to delete forecast and add bg

--- src/modules/view/mainView.js
+++ src/modules/view/mainView.js
@@ -1,10 +1,20 @@
+import changeBg from "./changeBg";
+
 class View {
   constructor() {}
 
-  showMainCard(position) {
+  showForecast(position) {
     const temp = document.getElementsByTagName("template")[position];
     const clone = temp.content.cloneNode(true);
     document.getElementsByClassName("container")[0].append(clone);
+  }
+  deleteForecast(parent) {
+    while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+    }
+  }
+  changeBackground(currentWeather) {
+    changeBg(currentWeather.weather[0].icon);
   }
 }
 
