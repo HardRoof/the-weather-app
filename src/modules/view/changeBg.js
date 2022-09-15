a75495e49e97757d89d6d3a8e0d4380f474550e9
HardRoof <mateus_grandez@hotmail.com>
2022-09-14 18:25:22 -0600

    Updated a few bg images

--- src/modules/view/changeBg.js
+++ src/modules/view/changeBg.js
@@ -1,18 +1,18 @@
 import clearSkyDay from "../../images/clear-sky/grooveland-designs-zjoydJb17mE-unsplash.jpeg";
-import clearSkyNight from "../../images/clear-sky/vince-sanga-LBQ1RVn5dfM-unsplash.jpg";
+import clearSkyNight from "../../images/clear-sky/kevin-buman-7YD8Q-3mEMg-unsplash.jpeg";
 import fewCloudsDay from "../../images/few-clouds/aziz-ayad-NJDyFoXDAus-unsplash.jpeg";
-import fewCloudsNight from "../../images/few-clouds/kym-mackinnon-wNSWuIjkD1I-unsplash.jpg";
+import fewCloudsNight from "../../images/few-clouds/sid-soni-D8VwM3N5xz4-unsplash.jpg";
 import scatteredCloudsDay from "../../images/scattered-clouds/nick-fewings-ZtDiJzokZi0-unsplash.jpeg";
 import scatteredCloudsNight from "../../images/scattered-clouds/herrmann-stamm-4vmjOLf9edk-unsplash.jpeg";
-import brokenCloudsDay from "../../images/broken-clouds/dave-hoefler-llEjCH71E9o-unsplash.jpg";
+//import brokenCloudsDay from "../../images/broken-clouds/raychel-sanner-cswvKS59E90-unsplash.jpeg";
 import brokenCloudsNight from "../../images/broken-clouds/anandu-vinod-pbxwxwfI0B4-unsplash.jpg";
-import showerRainDay from "../../images/shower-rain/ed-leszczynskl-R3ofE-8DyLk-unsplash.jpeg";
+import showerRainDay from "../../images/shower-rain/nic-y-c-CrXuYxfOJ4I-unsplash.jpg";
 import showerRainNight from "../../images/shower-rain/kyle-szegedi-JSmc0GmSV1o-unsplash.jpg";
 import rainDay from "../../images/rain/simon-kuznetsov-igQx3ETkKZY-unsplash.jpeg";
-import rainNight from "../../images/rain/anna-atkins-rNBaaxyeWWM-unsplash.jpg";
-import thunderstormDay from "../../images/thunderstorm/raychel-sanner-LCswFCnyE6Y-unsplash.jpg";
-import thunderstormNight from "../../images/thunderstorm/eugene-triguba-Cm5zI68Wdew-unsplash.jpg";
-import snowDay from "../../images/snow/nathan-fertig-_o_Nw3H21Js-unsplash.jpg";
+import rainNight from "../../images/rain/stormseeker-vRCZ3DztaoA-unsplash.jpg";
+import thunderstormDay from "../../images/thunderstorm/raychel-sanner-LCswFCnyE6Y-unsplash.jpg"; // too many details bottom part
+import thunderstormNight from "../../images/thunderstorm/michal-mancewicz-R9L7ukhBSgs-unsplash.jpeg";
+import snowDay from "../../images/snow/nathan-fertig-_o_Nw3H21Js-unsplash.jpg"; // too white/dark bottom
 import snowNight from "../../images/snow/alex-teplitz-I-Tg2H84mos-unsplash.jpg";
 import mistDay from "../../images/mist/chris-leipelt-kWd5ER2XpSg-unsplash.jpeg";
 import mistNight from "../../images/mist/daniil-onischenko-3C6DIGvZfEw-unsplash.jpeg";
@@ -38,7 +38,7 @@ export default function (iconCode) {
       setDailyContainerBG("dailyContainer--white");
       return changeMainBg(scatteredCloudsNight);
     case "04d":
-      setDailyContainerBG("dailyContainer--black");
+      setDailyContainerBG("dailyContainer--white");
       return changeMainBg(brokenCloudsDay);
     case "04n":
       setDailyContainerBG("dailyContainer--white");
@@ -50,7 +50,7 @@ export default function (iconCode) {
       setDailyContainerBG("dailyContainer--white");
       return changeMainBg(showerRainNight);
     case "10d":
-      setDailyContainerBG("dailyContainer--black");
+      setDailyContainerBG("dailyContainer--white");
       return changeMainBg(rainDay);
     case "10n":
       setDailyContainerBG("dailyContainer--white");
@@ -68,7 +68,7 @@ export default function (iconCode) {
       setDailyContainerBG("dailyContainer--white");
       return changeMainBg(snowNight);
     case "50d":
-      setDailyContainerBG("dailyContainer--black");
+      setDailyContainerBG("dailyContainer--white");
       return changeMainBg(mistDay);
     case "50n":
       setDailyContainerBG("dailyContainer--white");
@@ -82,6 +82,5 @@ function changeMainBg(image) {
 }
 
 function setDailyContainerBG(color) {
-  console.log(document.querySelectorAll(".dailyContainer"));
   document.querySelectorAll(".dailyContainer")[0].classList.add(color);
 }
