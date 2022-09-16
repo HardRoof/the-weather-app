b3f89daf68c4c7b97e5019a3282f7f741d4fa913
HardRoof <mateus_grandez@hotmail.com>
2022-09-15 18:52:16 -0600

    Formatted code

--- src/modules/model/mainModel.js
+++ src/modules/model/mainModel.js
@@ -8,48 +8,45 @@ class Model {
     this.imperial = "imperial";
   }
 
-  async getGeoCode(searchText) {
+  async fetchGeoLocation(searchText) {
     try {
       const URL = `http://api.openweathermap.org/geo/1.0/direct?q=${searchText}&appid=${this.key}`;
       const response = await fetch(URL, { mode: "cors" });
       const data = await response.json();
-      //console.log(data);
       return data[0];
     } catch (err) {
-      console.log(err, "Refresh page and try again later");
+      return console.log(err, "Refresh page and try again later");
     }
   }
 
-  async getCurrentWeather(lat, lon) {
+  async fetchCurrentWeather(lat, lon) {
     const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${this.key}&units=metric`;
     const response = await fetch(URL, { mode: "cors" });
     if (!response.ok) {
       throw Error(response.status);
     }
     const data = await response.json();
-    // console.log(data);
     return data;
   }
 
-  async getEightDaysWeather(lat, lon) {
+  async fetchEightDaysWeather(lat, lon) {
     const URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely,alerts&units=metric&appid=20f7632ffc2c022654e4093c6947b4f4`;
     const response = await fetch(URL, { mode: "cors" });
     if (!response.ok) {
       throw Error(response.status);
     }
     const data = await response.json();
-    //console.log(data);
     return data;
   }
 
   createMainCard(currentWeather, geoLocation) {
     const condition = currentWeather.weather[0].description;
     const regionNamesInEnglish = new Intl.DisplayNames(["en"], { type: "region" }); // To convert CA to Canada, per example
-    const city = geoLocation.name + ", " + regionNamesInEnglish.of(currentWeather.sys.country);
+    const city = `${geoLocation.name}, ${regionNamesInEnglish.of(currentWeather.sys.country)}`;
     const degrees = Math.round(currentWeather.main.temp);
     const feelsLike = Math.round(currentWeather.main.feels_like);
-    const wind = this.convertFromMs(currentWeather.wind.speed) + " km/h";
-    const humidity = "HUMIDITY: " + currentWeather.main.humidity + "%";
+    const wind = `${this.convertFromMs(currentWeather.wind.speed)} km/h`;
+    const humidity = `HUMIDITY: ${currentWeather.main.humidity}%`;
     const mainCard = this.generateMainCard(condition, city, degrees, feelsLike, wind, humidity);
     mainCard.fillMainCard();
   }
@@ -59,7 +56,7 @@ class Model {
   }
 
   generateMainCard(condition, city, degrees, feelsLike, wind, humidity) {
-    let mainCardItem = new MainCard(condition, city, degrees, feelsLike, wind, humidity);
+    const mainCardItem = new MainCard(condition, city, degrees, feelsLike, wind, humidity);
     return mainCardItem;
   }
 
@@ -69,7 +66,7 @@ class Model {
   }
 
   generateEightDaysCard(eightDaysWeather) {
-    let eightDaysCardItem = new EightDaysCard(eightDaysWeather);
+    const eightDaysCardItem = new EightDaysCard(eightDaysWeather);
     return eightDaysCardItem;
   }
 }
